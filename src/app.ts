a0d25bf965ae2d5962747f2f5d882a2e07d21058
sniekdhoshafiq <developershafiq9578@gmail.com>
2026-03-06 01:21:59 +0600

    Add PostgreSQL tables for Vehicle Rental System

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,13 @@
 import express, { Request, Response } from "express";
+import initDB from "./config/db";
 
 const app = express();
+
+// parser
+app.use(express.json());
+
+// initializing DB
+initDB();
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello from vehicle rental system");
