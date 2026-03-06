f732002fd7685d2f42c30ea80668064e66ef5aba
sniekdhoshafiq <developershafiq9578@gmail.com>
2026-03-07 00:15:39 +0600

    Complete user registration authentication part

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from "express";
 import initDB from "./config/db";
+import { userRoutes } from "./modules/users/user.route";
 
 const app = express();
 
@@ -8,6 +9,8 @@ app.use(express.json());
 
 // initializing DB
 initDB();
+
+app.use("/api/v1/auth", userRoutes);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello from vehicle rental system");
---
+++ src/modules/users/user.controller.ts
@@ -0,0 +1,27 @@
+import { Request, Response } from "express";
+import { userServices } from "./user.service";
+
+const signupUser = async (req: Request, res: Response) => {
+  try {
+    const result = await userServices.signupUser(req.body);
+
+    console.log("Result from user service: ", result);
+
+    const { password, ...userWithoutPassword } = result.rows[0];
+
+    res.status(201).json({
+      success: true,
+      message: "User registered successfully",
+      data: userWithoutPassword,
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+export const userControllers = {
+  signupUser,
+};
