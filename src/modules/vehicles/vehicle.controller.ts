c95bcf28bf065d04ae385336b1d518ee0ea21661
sniekdhoshafiq <developershafiq9578@gmail.com>
2026-03-07 02:26:55 +0600

    Implement get all vehicles endpoint

--- src/modules/vehicles/vehicle.controller.ts
+++ src/modules/vehicles/vehicle.controller.ts
@@ -18,6 +18,32 @@ const createVehicle = async (req: Request, res: Response) => {
   }
 };
 
+const getAllVehicles = async (req: Request, res: Response) => {
+  try {
+    const result = await vehicleServices.getAllVehicles();
+
+    if (result.rows.length === 0) {
+      return res.status(200).json({
+        success: true,
+        message: "No vehicles found",
+        data: [],
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "Vehicles retrieved successfully",
+      data: result.rows,
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
 export const vehicleController = {
   createVehicle,
+  getAllVehicles,
 };
--- src/modules/vehicles/vehicle.service.ts
+++ src/modules/vehicles/vehicle.service.ts
@@ -25,6 +25,13 @@ const createVehicle = async (payload: Record<string, unknown>) => {
   return result;
 };
 
+const getAllVehicles = async () => {
+  const result = await pool.query(`SELECT * FROM vehicles`);
+
+  return result;
+};
+
 export const vehicleServices = {
   createVehicle,
+  getAllVehicles,
 };
